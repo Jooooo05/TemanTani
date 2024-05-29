a22400ab884171289798f4880bfe41c3b8f2747a
Jooooo05 <pd12pd13@gmail.com>
2024-05-29 21:53:21 +0700

    Menambahkan Function Start Inten di WelcomeActivity dan StartedActivity

--- app/src/main/java/com/jonatan/temantani/view/StartedActivity.kt
+++ app/src/main/java/com/jonatan/temantani/view/StartedActivity.kt
@@ -1,12 +1,24 @@
 package com.jonatan.temantani.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jonatan.temantani.R
+import com.jonatan.temantani.databinding.ActivityStartedBinding
+import com.jonatan.temantani.view.welcome.WelcomeActivity
 
 class StartedActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartedBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_started)
+        binding = ActivityStartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonStarted.setOnClickListener(){
+            val inten = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/jonatan/temantani/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/jonatan/temantani/view/welcome/WelcomeActivity.kt
@@ -1,12 +1,26 @@
 package com.jonatan.temantani.view.welcome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jonatan.temantani.R
+import com.jonatan.temantani.databinding.ActivityWelcomeBinding
+import com.jonatan.temantani.view.login.LoginActivity
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignInWelcome.setOnClickListener(){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnSignUpWelcome.setOnClickListener(){
+//            val intent = Intent(this, )
+        }
     }
 }
